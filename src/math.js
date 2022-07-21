49c7bd5ad3db52d2f095f34c851e3b767dcd01a2
Ezmoneysniper42 <93779199+Ezmoneysniper42@users.noreply.github.com>
2022-07-21 10:59:07 +0700

    Initial commit

---
+++ src/math.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Pi = 3.1415926;
+
+function doublePi() {
+  return 6.2831852;
+}
+
+function triplePi() {
+  return 9.4247778;
+}
+
+export default Pi;
+export { doublePi, triplePi };
